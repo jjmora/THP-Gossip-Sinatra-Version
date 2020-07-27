623b7b2360b6566414d33614f0a2c8970ac09045
Juan Jose Mora <juanmora.dev@gmail.com>
2020-07-27 11:54:38 +0200

    2.5 ended -> Load all the gossips in index page

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,16 +1,22 @@
 # require 'bundler'  => Inutil si usamos el config.ru
 # Bundler.require    => Inutil si usamos el config.ru
+require 'gossip'
 
 class ApplicationController < Sinatra::Base
+
   get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
   end
 
   get '/gossips/new/'do
     erb :new_gossip
   end
 
-  
-
   #run! if app_file == $0 => Inutil si usamos el config.ru
+
 end
